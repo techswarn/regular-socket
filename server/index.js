5aab3cde0bc49d75ff36fd368bf074f344ae17d4
techswarn <tech.swarns@gmail.com>
2022-11-14 20:44:09 +0530

    code update

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ server.on('connection', socket => {
 
   socket.on('message', message => {
 
-    socket.send(`Roger that! ${message}`);
+    socket.send(`Message from server ${message}, Time: ${Date()}`);
 
   });
 
   setInterval(() => {
     console.log("ping")
     socket.ping();
-    // socket.send("heartbeat");
+    socket.send(`heartbeat,  Time: ${Date()}`);
   }, 10000);
 
 });
